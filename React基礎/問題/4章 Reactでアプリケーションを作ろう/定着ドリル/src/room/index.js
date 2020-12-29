67ccbc9bc704d924cdccf6f80d7acb63188e3977
kurahashi <koki.kurahashi@di-v.co.jp>
2020-12-29 20:44:58 +0900

    4-4定着

---
+++ React基礎/問題/4章 Reactでアプリケーションを作ろう/定着ドリル/src/room/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // スイッチの状態をstateで管理する 
+    };
+  }
+  render() {
+
+    const button_size = 100
+    return (
+      <div>
+        {/* 1つ目のスイッチ */}
+        <button style={{
+            height: button_size,
+            width: button_size,
+          }}onClick={() => {
+            // スイッチのON, OFFを切り替える
+          }}>
+          OFF {/* 1つ目のスイッチの状態 */}
+        </button>
+
+        {/* 2つ目のスイッチ */}
+        <button style={{
+          height: button_size,
+          width: button_size,
+        }} onClick={() => {
+          // スイッチのON, OFFを切り替える
+        }}>
+          OFF {/* 2つ目のスイッチの状態 */}
+        </button>
+      </div>
+    );
+  }
+}
