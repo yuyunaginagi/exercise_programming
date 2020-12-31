3e11b401ada74299e62f5a3efe1e353a74d8b278
kurahashi <koki.kurahashi@di-v.co.jp>
2020-12-31 11:13:57 +0900

    修正

--- Lesson4/問題/7章 Reactのライフサイクルを学ぼう/基礎問題/src/pages/home/index.tsx
+++ Lesson4/問題/7章 Reactのライフサイクルを学ぼう/基礎問題/src/pages/home/index.tsx
@@ -4,7 +4,11 @@ import { Link } from "react-router-dom";
 export default class Index extends React.Component {
   render() {
     return ( 
-      <div>
+      <div style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}>
         <h1>TechProduct</h1>     
         <Link to="/buy">
           <h3>購入ページへ</h3>
--- Lesson4/解答/7章 Reactのライフサイクルを学ぼう/基礎問題/src/pages/buy/index.tsx
+++ Lesson4/解答/7章 Reactのライフサイクルを学ぼう/基礎問題/src/pages/buy/index.tsx
@@ -6,50 +6,47 @@ interface State {
   ProductCount: number;
 }
 export default class Index extends React.Component<Props, State> {
-constructor(props: Props) {
-  super(props);
-  this.state = {
-      ProductCount: 0,
-  };
-}
-
-componentDidMount() {
-  alert("購入ページへ遷移します。");
-}
-
- handleOnClick = (): void => {
-  this.setState({ ProductCount: this.state.ProductCount + 1 })
-};
-
- componentWillUnmount() {
-  alert(
-    `商品を${this.state.ProductCount}個選択しました。`
-  );
-}
-
-componentDidUpdate(prevProps: Props, prevState: State) {
-  if (prevState.ProductCount !== this.state.ProductCount) {
-      if (this.state.ProductCount > 5 ){
-          alert("注意:選択できる個数は5個までです。");
-      }  
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+        ProductCount: 0,
+    };
   }
-}
-render() {
-  return (
-    <div>
-      <h1>TechProduct</h1>
-      <h2>携帯電話</h2>
-      <p>・スマートフォン</p>
-      <p>{this.state.ProductCount}個選択中</p>
-      <button
-        onClick={() => {
-            this.handleOnClick()
-        }}
-      >商品を1つ追加する</button>
-      <Link to="/">
-      <h3>トップページへ戻る</h3>
-      </Link>
-    </div>
-  );
-}
+
+  componentDidMount() {
+    alert("購入ページへ遷移します。");
+  }
+
+  handleOnClick = (): void => {
+    this.setState({ ProductCount: this.state.ProductCount + 1 })
+  };
+
+  componentWillUnmount() {
+    alert(
+      `商品を${this.state.ProductCount}個選択しました。`
+    );
+  }
+
+  render() {
+    return (
+      <div style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}>
+        <h1>TechProduct</h1>
+        <h2>携帯電話</h2>
+        <p>・スマートフォン</p>
+        <p>{this.state.ProductCount}個選択中</p>
+        <button
+          onClick={() => {
+              this.handleOnClick()
+          }}
+        >商品を1つ追加する</button>
+        <Link to="/">
+          <h3>トップページへ戻る</h3>
+        </Link>
+      </div>
+    );
+  }
 }
