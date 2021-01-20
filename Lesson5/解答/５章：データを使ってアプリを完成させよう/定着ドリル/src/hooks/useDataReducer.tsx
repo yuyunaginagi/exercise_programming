86f97761ff50eb249b28284ffe36ea953f2059e0
kurahashi <koki.kurahashi@di-v.co.jp>
2021-01-20 13:33:19 +0900

    問題作成

---
+++ Lesson5/解答/５章：データを使ってアプリを完成させよう/定着ドリル/src/hooks/useDataReducer.tsx
@@ -0,0 +1,32 @@
+import {useReducer} from "react";
+import { GenreType } from '../interfaces/GenreType'
+import { TaskType } from '../interfaces/TaskType'
+
+export type dataAction = {
+  type: "tasksUpdate" | "genresUpdate";
+  payload: { task?: TaskType[], genre?: GenreType[] }
+};
+
+export type Data = {
+  tasksData: TaskType[];
+  genresData: GenreType[];
+};
+
+export const useDataReducer = (): any => {
+  const initialData = {
+    tasksData: [],
+    genresData: [],
+  };
+
+  const reducer = (state: Data, action: dataAction) => {
+    switch (action.type) {
+      case "tasksUpdate":
+        return { ...state, tasksData: action.payload.task || state.tasksData }; // ...stateで事前に展開してオブジェクトのマージを行う
+      case "genresUpdate":
+        return { ...state, genresData: action.payload.genre || state.genresData};
+    }
+  };
+
+  const [data, dispatch] = useReducer(reducer, initialData);
+  return [data, dispatch];
+};
